45124afda1ddb2fa58ed5b1885657955831a344d
VICTOR HUGO DUARTE DO VALE <08008903457@tce.govrn>
2022-08-31 17:14:32 -0300

    insert com sqlite e criado UsersController

--- API/Startup.cs
+++ API/Startup.cs
@@ -18,12 +18,12 @@ namespace API
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        private readonly IConfiguration _config;
+        public Startup(IConfiguration config)
         {
-            Configuration = configuration;
+            _config = config;
+            
         }
-
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -36,7 +36,7 @@ namespace API
             });
             services.AddDbContext<DataContext>(options =>
             {
-                options.UseSqlite("Connection string");
+                options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
         }
 
